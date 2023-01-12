c5e31f1a748c60f12280ec93e383b13f1537bd9d
guanxi <guanxi@163.com>
2023-01-12 19:52:29 +0800

    init

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
 // 自动引入加载 router
-const { koaBody } = require('koa-body')
-const json = require('koa-json')
-const fs = require('fs')
-const path = require('path')
-const Router = require('@koa/router')
+const { koaBody } = require("koa-body");
+const json = require("koa-json");
+const fs = require("fs");
+const path = require("path");
+const Router = require("@koa/router");
 
 class AutoRouter {
-  app
   // 单例模式
-  static autoRouter = new AutoRouter()
+  static autoRouter = new AutoRouter();
   // 初始化方法
   init(app) {
-    this.app = app
-    const fileArr = this.getFileArr()
-    const rootRouter = this.getRootRouter()
-    this.loaderRouter(rootRouter, fileArr)
+    this.app = app;
+    const fileArr = this.getFileArr();
+    const rootRouter = this.getRootRouter();
+    this.loaderRouter(rootRouter, fileArr);
     // 加载全部路由
-    this.app.use(rootRouter.routes())
+    this.app.use(rootRouter.routes());
   }
   // 获取路由路径数组
   getFileArr() {
-    const fileArr = []
-    const fullPath = path.join(process.cwd(), 'src/router')
-    const files = fs.readdirSync(fullPath)
+    const fileArr = [];
+    const fullPath = path.join(process.cwd(), "src/router");
+    const files = fs.readdirSync(fullPath);
     files.forEach((item) => {
-      const fileItem = fullPath + '\\' + item
-      fileArr.push(fileItem)
-    })
-    return fileArr
+      const fileItem = fullPath + "\\" + item;
+      fileArr.push(fileItem);
+    });
+    return fileArr;
   }
   // 获取一级路由
   getRootRouter() {
     const rootRouter = new Router({
-      prefix: '/run',
-    })
-    this.app.use(koaBody())
-    this.app.use(json())
-    return rootRouter
+      prefix: "/run",
+    });
+    this.app.use(koaBody());
+    this.app.use(json());
+    return rootRouter;
   }
   // 加载路由
   loaderRouter(rootRouter, fileArr) {
     fileArr.forEach((item) => {
-      const module = require(item)
-      rootRouter.use(module.routes(), module.allowedMethods())
-    })
+      const module = require(item);
+      rootRouter.use(module.routes(), module.allowedMethods());
+    });
   }
 }
 
-module.exports = AutoRouter.autoRouter
+module.exports = AutoRouter.autoRouter;
